c19b0e482639eb589300818fc5beb7c4191c31ed
Topher Cawlfield <ccawlfield@gogoair.com>
2017-10-12 12:01:44 -0600

    Adding Issue (1-D)

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,34 @@ extern crate rand;
 extern crate lazy_static;
 //#[macro_use(s)]
 extern crate ndarray;
-use ndarray::{Array2, Array3};
+use ndarray::{Array2};
 
 mod simvote;
 
 use simvote::*;
 
+const CITIZENS: usize = 5;
+const CANDIDATES: usize = 2;
+
 fn main() {
     let mut rng = rand::thread_rng();
 
-    let axes: [&Consideration; 1] = [
+    let axes: [&Consideration; 2] = [
         &Likability{
             stretch_factor: 1.0,
         },
-//        Issue{
-//            sigma: 2.0,
-//        }
+        &Issue{
+            sigma: 2.0,
+        }
     ];
-    let mut scores = unsafe { Array2::uninitialized((5, 2)) };
+    let mut net_scores: Array2<f64> = Array2::zeros((CITIZENS, CANDIDATES));
+    let mut scores = unsafe { Array2::uninitialized((CITIZENS, CANDIDATES)) };
     for ax in axes.iter() {
         ax.gen_scores(&mut scores, &mut rng);
+        println!("scores:\n{:?}", scores);
+        for (sc, nsc) in scores.iter().zip(net_scores.iter_mut()) {
+            *nsc += *sc;
+        }
     }
-    println!("scores: {:?}", scores);
+    println!("Net scores:\n{:?}", net_scores);
 }
--- src/simvote.rs
+++ src/simvote.rs
@@ -16,11 +16,11 @@ pub struct Likability {
     pub stretch_factor: f64,
 }
 
-impl Likability {
-    fn gen_cand_like<R: Rng>(&self, rng: &mut R) -> f64 {
-        NORMAL.ind_sample(rng) * self.stretch_factor
-    }
-}
+// impl Likability {
+//     fn gen_cand_like<R: Rng>(&self, rng: &mut R) -> f64 {
+//         NORMAL.ind_sample(rng) * self.stretch_factor
+//     }
+// }
 
 impl Consideration for Likability {
     fn gen_scores(&self, scores: &mut Array2<f64>, mut rng: &mut ThreadRng) {
@@ -28,16 +28,43 @@ impl Consideration for Likability {
         // All citizens are the same in this regard.
         // Or at least we assume there are enough citizens that every representative
         // group in position-space spans all degrees of likability alignment.
-        let mut candidates = Vec::with_capacity(ncand);
+        // let mut candidates = Vec::with_capacity(ncand);
+        // for _ in 0..ncand {
+        //     candidates.push(self.gen_cand_like(&mut rng));
+        // }
         for i in 0..ncand {
-            candidates.push(self.gen_cand_like(&mut rng));
-        }
-        for j in 0..ncit {
-            for i in 0..ncand {
-                *scores.get_mut((j, i)).unwrap() = *candidates.get(i).unwrap();
+            let cand_like = NORMAL.ind_sample(rng) * self.stretch_factor;
+            for j in 0..ncit {
+                //*scores.get_mut((j, i)).unwrap() = *candidates.get(i).unwrap();
+                *scores.get_mut((j, i)).unwrap() = cand_like;
             }
         }
     }
 }
 
 //////////////////////////////////////////
+
+pub struct Issue {
+    pub sigma: f64,
+}
+
+impl Consideration for Issue {
+    fn gen_scores(&self, scores: &mut Array2<f64>, mut rng: &mut ThreadRng) {
+        let (ncit, ncand) = scores.dim();
+        // All citizens are the same in this regard.
+        // Or at least we assume there are enough citizens that every representative
+        // group in position-space spans all degrees of likability alignment.
+        let mut cand_position = Vec::with_capacity(ncand);
+        for _ in 0..ncand {
+            cand_position.push(NORMAL.ind_sample(rng) * self.sigma);
+        }
+        for j in 0..ncit {
+            let cit_position = NORMAL.ind_sample(rng) * self.sigma;
+            for i in 0..ncand {
+                *scores.get_mut((j, i)).unwrap() =
+                    -(*cand_position.get(i).unwrap() - cit_position).powi(2);
+                    //-(*cand_position.get(i).unwrap() - cit_position).abs(2);
+            }
+        }
+    }
+}
